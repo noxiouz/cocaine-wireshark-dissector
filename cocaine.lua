7e1e2c7a752b377ec86af0d15284016208a3a18f
Anton Tyurin <noxiouz@yandex.ru>
2014-06-04 18:04:01 +0400

    tiny fix

--- cocaine.lua
+++ cocaine.lua
@@ -17,9 +17,6 @@ end
 function cocaine.init()
 end
 
--- register a chained dissector for port 8888
 local tcp_dissector_table = DissectorTable.get("tcp.port")
 dissector = tcp_dissector_table:get_dissector(10053)
-  -- you can call dissector from function cocaine.dissector above
-  -- so that the previous dissector gets called
 tcp_dissector_table:add(10053, cocaine)
